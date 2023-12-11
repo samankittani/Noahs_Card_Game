f54a29b7983b5927ce578784ef4df16a83a31bf7
nrs4q <samankittani@gmail.com>
2023-12-11 01:56:14 -0600

    added setcolor function to cards for value cards

--- cardMorph.js
+++ cardMorph.js
@@ -36,13 +36,28 @@ cardMorph.prototype.init = function(value, suit, cost) {
 cardMorph.prototype.setImage = function() {
   if(this.value > 0){
     this.display.addChild(this.display.text = new StringMorph(`${this.value}`, 70, 'times', true))
-    this.display.text.setPosition(this.display.topLeft())
+    this.display.text.setPosition(this.display.topLeft());
+    this.setColor();
+ 
   }
   else if(this.suit === SUITS.spade){
     this.display.texture = 'http://localhost:8000/images/newMoon.svg';
   }
   else if(this.suit === SUITS.club){
     this.display.texture = 'http://localhost:8000/images/NewBlackhole.svg';
+  }
+}
+/* 
+this function is used to select the right color.
+used only for the number cards 
+TODO: pick good rgb values for the value cards, 
+or get an SVG that looks good without taking away
+from the number*/
+cardMorph.prototype.setColor = function () {
+  if(this.suit === SUITS.spade){
+    // this.display.color = new Color( 89, 4, 4, 1);
+  }else if(this.suit === SUITS.club){
+    // this.display.color = new Color(1, 1, 1, 1);
   }
 }
 
